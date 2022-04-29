0f2d7ae4d8374294c092b646118edf017c214d4a
Kota <maedakoutaa@gmail.com>
2022-04-30 00:32:29 +0900

    commit

--- MiniApp49-PKHUD/ViewController.swift
+++ MiniApp49-PKHUD/ViewController.swift
@@ -6,12 +6,44 @@
 //
 
 import UIKit
+import WebKit
+import PKHUD
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    @IBOutlet private var webView: WKWebView!
+    private let urlString = "https://apple.com"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let webConfig = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfig)
+        webView.navigationDelegate = self
+        webView.uiDelegate = self
+        view = webView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        HUD.dimsBackground = true
+        HUD.show(.progress, onView: view)
+
+        if let url = URL(string: urlString) {
+            self.webView.load(URLRequest(url: url))
+        } else {
+            print("URLが取得できませんでした。")
+        }
+    }
+
+
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("読み込み開始")
+    }
+
+    // MARK: - 読み込み完了
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("読み込み完了")
+        HUD.hide(animated: true)
     }
 
 
---
+++ Pods/Target Support Files/Pods-MiniApp49-PKHUD/Pods-MiniApp49-PKHUD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MiniApp49_PKHUD : NSObject
+@end
+@implementation PodsDummy_Pods_MiniApp49_PKHUD
+@end
